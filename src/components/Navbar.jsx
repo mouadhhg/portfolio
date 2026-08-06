42735e62ffb318836e6d30174a522b0318baebc4
mouadhhg <zfanmadh@gmail.com>
2026-08-06 19:28:38 +0100

    update whatsapp phone number

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ export default function Navbar() {
           <div className="flex space-x-6 pt-4 border-t w-full justify-center">
              <a href="https://www.instagram.com/sharp_edge_studio.dev?igsh=MTR4cGM1dmp1dXllMA==" className="text-gray-300 hover:text-pink-600"><FaInstagram size={22} /></a>
               <a href="https://www.tiktok.com/@sharpedgestudio?_r=1&_d=em96i73j02j4bi&sec_uid=MS4wLjABAAAA4Nc6Zp7aC-_E0YNNdGA_zrnwcr0AkXT061SqF1F6kKMSkkAiOBpjiLuRITAgMpNj&share_author_id=7667998371017786376&sharer_language=en&source=h5_m&u_code=f4khik3845lff3&timestamp=1785442492&user_id=7667998371017786376&sec_user_id=MS4wLjABAAAA4Nc6Zp7aC-_E0YNNdGA_zrnwcr0AkXT061SqF1F6kKMSkkAiOBpjiLuRITAgMpNj&item_author_type=1&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7668225254511347474&share_link_id=e5e3006c-18b4-4e25-aaf0-8781ddfc307c&share_app_id=1233&ugbiz_name=ACCOUNT&ug_btm=b8727%2Cb4907&social_share_type=5&share_enter_from=settings_page&item_author_type=1&enable_checksum=1" className="text-gray-500 hover:text-black"><FaTiktok size={22} /></a>
-             <a href="https://wa.me/21290231048" className="text-gray-300 hover:text-green-600"><FaWhatsapp size={22} /></a>
+             <a href="https://wa.me/212690231048" className="text-gray-300 hover:text-green-600"><FaWhatsapp size={22} /></a>
           </div>
         </div>
       </div>
